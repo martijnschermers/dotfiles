c71ab5adf0875a9849b115496456b1ee6db65a05
Martijn <martijnschermers.dev@gmail.com>
2023-01-03 19:49:15 +0100

    add: lualine plugin

--- lua/martijn/packer.lua
+++ lua/martijn/packer.lua
@@ -50,5 +50,8 @@ return require('packer').startup(function(use)
         end
     }
 
-    use('rebelot/heirline.nvim')
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
 end)
